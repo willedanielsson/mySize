23b9ae6afb2f2e7556fc557c2b5d9194477b0d58
William Danielsson <william.danielsson@netlight.com>
2016-11-27 13:46:57 +0100

    Added login with Google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,42 @@
 import { Component } from '@angular/core';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { AngularFire, FirebaseListObservable, AuthProviders } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+	user = {};
 	items: FirebaseListObservable<any[]>;
-	console.log(af.database.list('/items'));
-	constructor(af: AngularFire) {
-		this.items = af.database.list('/items');
-  	}
+	constructor(public af: AngularFire) {
+		this.af.auth.subscribe(user => {
+			if(user) {
+				// Logged in
+				console.log("Authenticate: Logged in");
+				console.log(user);
+				this.user = user;	
+				this.items = af.database.list('/items');
+			} else {
+				// Not logged in
+				console.log("Authenticate: Not");
+				this.user = {}
+			}	
+		})
+	}
+
+	login() {
+		console.log("Login");
+		this.af.auth.login({
+	    	provider: AuthProviders.Google
+	  	});
+	}
+	 
+	logout() {
+		console.log("Logout");
+	  	this.af.auth.logout();
+	}
+
   	title = "It still works";
-  //title = 'app works!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { AngularFireModule } from 'angularfire2';
+import { AngularFireModule, AuthMethods, AuthProviders  } from 'angularfire2';
 
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Firebase config 
 export const firebaseConfig = {
-  apiKey: 'APIKEY',
-  authDomain: 'DOMAIN',
-  databaseURL: 'DBURL',
-  storageBucket: 'BUCKET',
-  messagingSenderId: "SENDERID"
+  apiKey: 'AIzaSyCcJFMOq9fChyexiikIppNkZjVKZNQSyB8',
+  authDomain: 'mysize-aebce.firebaseapp.com',
+  databaseURL: 'https://mysize-aebce.firebaseio.com',
+  storageBucket: 'mysize-aebce.appspot.com',
+  messagingSenderId: "139935871422"
 };
 
 @NgModule({
@@ -20,7 +21,10 @@ export const firebaseConfig = {
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireModule.initializeApp(firebaseConfig, {
+      provider: AuthProviders.Google,
+      method: AuthMethods.Popup
+    }),
     FormsModule,
     HttpModule
   ],
